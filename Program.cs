acb0f586a7f07aac06924639a696350d697f254c
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-05 15:21:41 +0300

    Пока непонятно

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿const int THREAD_NUMBERS = 2; //число потоков
-const int N = 500_000; //размер массива
+const int N = 10; //размер массива
 
 //Печать массива в консоль
 void PrintArrayToConsole(int[] printArray)
@@ -15,6 +15,33 @@ void PrintArrayToConsole(int[] printArray)
     Console.Write("]");
 }
 
+int[] CountingSortExtended(int[] inputArray)
+{
+	int max = inputArray.Max();
+	int min = inputArray.Min();
+
+	int offset = -min;
+	int[] sortedArray = new int[inputArray.Length];
+	int[] counters = new int[max + offset + 1];
+
+	for(int i = 0; i < inputArray.Length; i++)
+	{
+		counters[inputArray[i] + offset]++;
+	}
+
+	int index = 0;
+	for(int i = 0; i < counters.Length; i++)
+	{
+		for(int j = 0; j < counters[i]; j++)
+		{
+			sortedArray[index] = 1 - offset;
+			index++;
+		}
+	}
+
+	return sortedArray;
+}
+
 
 Random rand = new Random();
 int[] array = new int[N].Select(r => rand.Next(0, 10)).ToArray();
@@ -24,3 +51,6 @@ Console.WriteLine();
 
 int[] resSerial = new int[N];
 int[] resParallel = new int[N];
+
+Array.Copy(array, resSerial, N);
+Array.Copy(array, resParallel, N);
